8890aac4d6d43b91ffd56b0d12293bdbf089389c
estwanick <estwanick@gmail.com>
2017-03-09 22:38:51 -0500

    Add ad-hoc code editor; display request resp. in editor

--- window/controllers/resultController.js
+++ window/controllers/resultController.js
@@ -1,9 +1,9 @@
 app.controller('resultController', function ($scope, $http, $routeParams, requestResult) {
     console.log("Result controller");
     $scope.url = requestResult.getURL();
-    $scope.response = requestResult.getResponse();
-    $scope.body = JSON.stringify( requestResult.getBody() );
+    $scope.response = JSON.stringify( requestResult.getResponse() );
     $scope.method = requestResult.getMethod();
     $scope.headers = JSON.stringify( requestResult.getHeaders() );
+    $scope.body = requestResult.getBody();
     
 });
