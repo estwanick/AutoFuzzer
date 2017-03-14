f58c4c04ba9093be17858a8e3662cd32f5b6470f
estwanick <estwanick@gmail.com>
2017-03-14 19:05:49 -0400

    Display XML & JSON response formatted

--- window/controllers/resultController.js
+++ window/controllers/resultController.js
@@ -1,13 +1,21 @@
+function getContentType(content, contentType){
+    if(contentType.includes("json")){
+        return vkbeautify.json(content, 4);
+    }else if(contentType.includes("xml")){
+        return vkbeautify.xml(content, 4);
+    }else if(contentType.includes("html")){
+        return "html";
+    }else{
+        return "text";
+    }
+}
+
 app.controller('resultController', function ($scope, $http, $routeParams, requestResult) {
     console.log("Result controller");
     $scope.url = requestResult.getURL();
     $scope.method = requestResult.getMethod();
     $scope.headers = JSON.stringify( requestResult.getHeaders() );
-    $scope.body = requestResult.getBody();
-    //let contentType = requestResult.getContentType();
-    //console.log( contentType );
-
-    $scope.response = requestResult.getResponse();
-    //$scope.response = vkbeautify.xml( requestResult.getResponse(), 4);
+    $scope.body = requestResult.getBody(); //Should know the type of the body
+    $scope.response = getContentType(requestResult.getResponse(), requestResult.getContentType());
 
 });
