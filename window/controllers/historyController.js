9ebf312d6dec5828549fc754dbc6cdb40fd0fb43
estwanick <estwanick@gmail.com>
2017-03-19 23:06:34 -0400

    Add navigation from history to result page

--- window/controllers/historyController.js
+++ window/controllers/historyController.js
@@ -1,8 +1,18 @@
-app.controller('historyController', function ($scope, $http, requestHistory) {
+app.controller('historyController', function ($scope, $http, $location, requestHistory, requestResult) {
     let recentHistory = requestHistory.getHistory().slice().reverse();
     $scope.historyList = recentHistory;
     $scope.clearHistory = function (){
         requestHistory.setHistory([]);
         $scope.historyList = [];
+    };
+    $scope.viewResult = function(oRequest){
+        requestResult.setURL(oRequest.url);
+        requestResult.setMethod(oRequest.method);
+        requestResult.setBody(oRequest.body);
+        requestResult.setResponse(oRequest.data);
+        requestResult.setHeaders(oRequest.headers);
+        requestResult.setContentType(oRequest.contentType);
+        let path = "result/" + oRequest.url;
+        $location.path(path);
     };
 });
