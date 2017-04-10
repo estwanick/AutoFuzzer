fb8e80b5615bceaf761cdfe7bef83784d9cb63fd
estwanick <estwanick@gmail.com>
2017-04-10 19:00:22 -0400

    Remove Chrome App Wrapper (Chrome no longer accepting apps)

--- background/background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window/index.html', {
-    'outerBounds': {
-      "width" :   window.screen.availWidth,
-      "height":   window.screen.availHeight 
-    }
-  });
-});
